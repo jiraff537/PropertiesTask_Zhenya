4849b8b2646b8d8594b7981e462608047e5c2d8e
i5 6400 <neco00cun@gmail.com>
2023-09-21 22:36:14 +0200

    added html constructor(working)

--- base/src/main/java/org/example/Main.java
+++ base/src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import static j2html.TagCreator.*;
 public class Main {
     public static void main(String[] args) {
         System.out.print(body(
-                h1("Hello, World!"),
-                img().withSrc("/img/hello.png")
+                h1("Hello, World!")
         ).render());
     }
 }
