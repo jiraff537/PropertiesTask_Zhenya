0e5a0e10f455ed572c92f1bbc80c1c9e2d5de3fc
i5 6400 <neco00cun@gmail.com>
2023-09-24 20:47:55 +0200

    restrt edited prop reader started fixes v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,18 @@
 package org.example;
-import org.example.Reader;
+import org.example.PropertyReader;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Properties config = Reader.newPropFile("files/ii.properties");
-        for (int i = 0; i < 6; i++){
-            System.out.println(Reader.getFromIndex(config, i));
+        Properties config = PropertyReader.loadProperties("files/ii.properties");
+        List<String> keys = PropertyReader.getPropertyKeys(config);
+        for (int i = 0; i < Math.min(keys.size(), 6); i++) {
+            String key = keys.get(i);
+            String value = PropertyReader.getValueForKey(config, key);
+            System.out.println(key + ": " + value);
         }
-
     }
 }
